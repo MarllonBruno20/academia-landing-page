3e21585c4e168d4aef40ae640b38f8b7ef3c3efb
MarllonBruno20 <marllon.bruno2002@gmail.com>
2024-10-11 21:59:14 -0300

    Commit inicial dos arquivos

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-blue-500 h-screen text-white">
+      <h1 className="text-3xl font-bold">Hello, world!</h1>
+    </div>
+  );
+}
+
+export default App;
